b73e59b07fb484a3586220dacb961d17bcbc43d7
JessicaHerford <jrherford12@gmail.com>
2019-11-20 16:25:06 -0700

    4th day of flask, created simple 4 page site with nav

---
+++ 4-flask/program/routes.py
@@ -0,0 +1,23 @@
+from program import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/dogs')
+def dogs():
+    return render_template("dogs.html")
