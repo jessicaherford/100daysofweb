9b7c003e86bfc4526600ce41cf90a873030fcba7
JessicaHerford <jrherford12@gmail.com>
2019-12-03 18:57:53 -0700

    Day 17 added chuck norris page to diplay jokes from api

--- 17-20-flask-api/program/routes.py
+++ 17-20-flask-api/program/routes.py
@@ -1,6 +1,10 @@
+import requests
+import json
+
 from program import app
 from flask import render_template
 from datetime import datetime
+
 
 timenow = datetime.today()
 
@@ -9,3 +13,12 @@ timenow = datetime.today()
 @app.route('/index')
 def index():
     return render_template('index.html', time=timenow)
+
+
+@app.route('/chuck')
+def chuck():
+    joke = requests.get('https://api.chucknorris.io/jokes/random')
+    data = joke.json()
+    return render_template('chuck.html', chuck_joke=data['value'])
+
+
