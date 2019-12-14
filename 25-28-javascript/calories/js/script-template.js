9e175dbb20d66f9b6e941d432ba988d88094a81a
JessicaHerford <jrherford12@gmail.com>
2019-12-14 16:48:09 -0700

    Day 28 more javascript

--- 25-28-javascript/calories/js/script-template.js
+++ 25-28-javascript/calories/js/script-template.js
@@ -43,25 +43,75 @@ if (form.attachEvent) {
 
 // helpers
 function recalculateTotal(){
-  // get all table cells (tds) and sum the calories = td with kcal
+    let tds = document.getElementsByTagName("td");
+    let sum = 0, len = tds.length;
+    for (var i = 0; i<len; i++) {
+        if (tds[i].className == "kcal") {
+            sum += parseFloat(tds[i].innerText);
+        }
+    }
+  return sum;
 }
 
 function updateTotalKcal(){
   // write the total kcal count into  the total id, if 0 hide the
-  // foodBalanceWrapper div
+  let sum = recalculateTotal();
+  document.getElementById("total").innerHTML = parseInt(sum);
+
+  // if 0 kcal hide kcal div
+  if(sum == 0){
+    foodBalanceWrapper.style.display = "none";
+  } else {
+    foodBalanceWrapper.style.display = "block";
+  }
 }
 
 function emptyFoodPicker(){
-  // reset the foodPicker ID value
+  document.getElementById("foodPicker");
 }
 
 function removeRow(){
-  // remove a table row and update the total kcal
-  // https://stackoverflow.com/a/53085148
+  let td = event.target.parentNode;
+  let tr = td.parentNode; // the row to be removed
+  tr.parentNode.removeChild(tr);
+  updateTotalKcal();
 }
 
 function updateFoodLog(){
-  // udate the food table with the new food, building up the inner dom
-  // elements, including adding a delete button / onclick handler
-  // finally call updateTotalKcal and emptyFoodPicker
+  let selectedFood = document.getElementById("foodPicker").value;
+  selectedFood = selectedFood.substr(0, selectedFood.lastIndexOf("(")).trim();
+  let kcals = foodDb[selectedFood];
+
+  if(kcals === undefined){
+    alert('Unknown food, please try again');
+    emptyFoodPicker();
+    return;
+  }
+
+  let tbody = document.getElementById("foodBalanceBody");
+  let newTableRow = document.createElement("tr");
+
+  let col1 = document.createElement("td");
+  let col2 = document.createElement("td");
+  let col3 = document.createElement("td");
+
+  col1.textContent = selectedFood;
+
+  col2.textContent = parseInt(kcals);
+  col2.className = "kcal";  // for calculation
+
+  let deleteBtn = document.createElement("input");
+  deleteBtn.onclick = removeRow;
+  deleteBtn.type = 'button';
+  deleteBtn.className = 'delete';
+  col3.appendChild(deleteBtn);
+
+  newTableRow.appendChild(col1);
+  newTableRow.appendChild(col2);
+  newTableRow.appendChild(col3);
+
+  tbody.appendChild(newTableRow);
+
+  updateTotalKcal();
+  emptyFoodPicker();
 }
