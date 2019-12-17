2b4a277da5bc4b97120a6e952c10e51e0a94f3a2
JessicaHerford <jrherford12@gmail.com>
2019-12-16 18:16:24 -0700

    Day 30 changed to different theme

--- 29-32-static-sites/pelicanconf.py
+++ 29-32-static-sites/pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'America/Denver'
 
 DEFAULT_LANG = 'en'
 
+THEME = "../../../pelican-themes/Flex"
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
