6048f907bb73729699e7820dcb9285454fb3cc1a
JessicaHerford <jrherford12@gmail.com>
2019-12-17 16:21:48 -0700

    Day 31 added about us page and another blog post

--- 29-32-static-sites/pelicanconf.py
+++ 29-32-static-sites/pelicanconf.py
@@ -23,6 +23,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('About', 'about.html'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
          ('You can modify those links in your config file', '#'),)
